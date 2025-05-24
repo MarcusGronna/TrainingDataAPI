082164dcd3e37d1d889e0cd079d92d47b0e962bd
Marcus <mgronna@gmail.com>
2025-05-24 12:55:58 +0200

    updated cors

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://trainingdataapi.onrender.com"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
