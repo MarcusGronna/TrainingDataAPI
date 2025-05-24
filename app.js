c4ca1db0f5b76e1575a78212e25b3b06aacfd8d7
Marcus <mgronna@gmail.com>
2025-05-24 07:05:43 +0200

    Empty

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var mongoose = require("mongoose");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var exercisesRouter = require("./routes/exercises");
@@ -23,6 +24,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
